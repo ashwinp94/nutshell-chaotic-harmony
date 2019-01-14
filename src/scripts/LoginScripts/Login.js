992a45d1c5ca35bf11757081ba963bad00e837fd
dek <deeeqhaji@gmail.com>
2019-01-13 19:05:44 -0600

    added new register form and the login page is working now

--- src/scripts/LoginScripts/Login.js
+++ src/scripts/LoginScripts/Login.js
@@ -20,37 +20,40 @@ const Login = {
       <input class="email__Input" type="text" name="email">
     </div>
     <button class="button--login">Login</button>
-    <button class="button--account">Sign up</button>
   </section>`
+  
     },
     // after when you click the register button, a new form that have email and password inputwill populate the dom
     signUp() {
 
-        let registerDom = document.querySelector("#usersOutput");
-        let registerAccount = document.querySelector(".button--account");
-        registerAccount.addEventListener("click", function () {
-            registerDom.innerHTML = ` <form>
+        let registerAccount = document.querySelector("#usersOutput");
+        registerAccount.innerHTML =  `<h3> Sign Up</h3> </br>
+        <p> It’s free and always will be.</p>
+        <form>
         <div class="dek__usernamePrompt">
-        New username: 
-      </div>
-      <div class="usernameInput">
-        <input type="text" name="username">
-      </div>
-      <div class="emailPrompt">
-          Email:
-      </div>
-      <div class="emailInput"> 
-       <input type="text" name="email"></br>
-       <button class="newAccount">Register</button>
-       </form>`
-        })
+         New username: 
+       </div>
+       
+        <input class="usernameInput"type="text" name="username">
+       </div>
+       <div class="emailPrompt">
+           Email:
+       </div>
+       
+        <input class="emailInput"type="text" name="email"></br>
+        <button class="newAccount">Register</button>
+        </form>`
+       
+        
     },
+
     // giving variable name of user input and email input
-    testEvent(){
+    testEvent() {
         let submitButton = document.querySelector(".button--login");
         submitButton.addEventListener("click", Login.getUserData)
-},
-getUserData() {
+    },
+
+    getUserData() {
         let username = document.querySelector(".username__Input").value;
         let email = document.querySelector(".email__Input").value;
         Data.getData("users")
@@ -58,11 +61,26 @@ getUserData() {
                 allUsers.forEach(user => {
                     if (username === user.username && email === user.email) {
                         console.log(user.username)
-                        console.log(`this is: ${user.id}`)
-                    }
+                        alert(` ${user.username} is success`)
+                    } 
+                    
                 });
             })
 
+    },
+    registerUser() {
+        let newUser = document.querySelector(".newAccount")
+        newUser.addEventListener("click", Login.registerData)
+    },
+    registerData() {
+        let newUser = document.querySelector(".usernameInput").value;
+        let newEmail = document.querySelector(".emailInput").value;
+           // add user input into an object that will be passed into the database
+           let newEntry = {
+               username : newUser,
+               email : newEmail
+           };
+           Data.postNewUser(newEntry)
     }
 }
 
